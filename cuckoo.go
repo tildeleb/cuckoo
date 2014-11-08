f44a81f91f403eec53429f0d2d6ab476cb190ad3
Lawrence E. Bakst <leb@me.com>
2014-11-08 03:02:08 -0500

    first working version

--- cuckoo.go
+++ cuckoo.go
@@ -114,7 +114,7 @@ func (c *Cuckoo) Lookup(key Key) (Value, bool) {
 	for t, _ := range c.tbs {
 		b := c.hs[t]
 		for s, _ := range c.tbs[t][b] {
-			fmt.Printf("Lookup: key=%d, table=%d, bucket=%d, slot=%d, found key=%d\n", key, t, b, s, c.tbs[t][b][s].key)
+			//fmt.Printf("Lookup: key=%d, table=%d, bucket=%d, slot=%d, found key=%d\n", key, t, b, s, c.tbs[t][b][s].key)
 			if c.tbs[t][b][s].key == key {
 				//fmt.Printf("Lookup: key=%d, value=%d, table=%d, bucket=%d, slot=%d\n", key, val, t, b, s)
 				return c.tbs[t][b][s].val, true
@@ -125,28 +125,28 @@ func (c *Cuckoo) Lookup(key Key) (Value, bool) {
 }
 
 func (c *Cuckoo) Verify(base, n int) bool {
-	fmt.Printf("Verify: base=%d, n=%d \n", base, n)
+	//fmt.Printf("Verify: base=%d, n=%d \n", base, n)
 	cnt := 0
-	for i := base + 1; i <= base + n; i++ {
+	for i := base; i < base + n; i++ {
 		cnt++
 		v, ok := c.Lookup(Key(i))
 		if !ok {
 			fmt.Printf("Verify: lookup FAILED i=%d, cnt=%d\n", i, cnt)
 			return false
 		}
-		fmt.Printf("Verify: check i=%d, cnt=%d != v=%d\n", i, cnt, uint32(v))
+		//fmt.Printf("Verify: check i=%d, cnt=%d != v=%d\n", i, cnt, uint32(v))
 		if uint32(cnt) != uint32(v) {
 			fmt.Printf("Verify: FAIL i=%d, cnt=%d != v=%d\n", i, cnt, uint32(v))
 			return false
 		}
 	}
-	fmt.Printf("Verify: OK\n")
+	//fmt.Printf("Verify: OK\n")
 	return true
 }
 
 func (c *Cuckoo) insert(key Key, val Value, level int) (ok bool) {
 	var ins func(kx Key, vx Value) bool // forwqrd declare the closure so we can call it recursively
-	var phv = func() {
+	var _ = func() {
 		fmt.Printf("<")
 		for k, v := range c.hs {
 			if k != 0 {
@@ -178,18 +178,18 @@ func (c *Cuckoo) insert(key Key, val Value, level int) (ok bool) {
 	}
 	ins = func(kx Key, vx Value) bool {
 		calcHashes(kx)
-		fmt.Printf("Insert: level=%d, key=%d, ", level, kx)
-		phv()
+		//fmt.Printf("Insert: level=%d, key=%d, ", level, kx)
+		//phv()
 		k := kx
 		v := vx
 		for t, _ := range c.tbs {
-			phv()
+			//phv()
 			b := c.hs[t]
-			fmt.Printf("Insert: next table, level=%d, key=%d, value=%d, table=%d, bucket=%d\n", level, k, v, t, b)
+			//fmt.Printf("Insert: next table, level=%d, key=%d, value=%d, table=%d, bucket=%d\n", level, k, v, t, b)
 			for s, _ := range c.tbs[t][b] {
 				if c.tbs[t][b][s].key == 0 {
 					c.tbs[t][b][s].key, c.tbs[t][b][s].val = k, v
-					fmt.Printf("Insert: level=%d, key=%d, value=%d, table=%d, bucket=%d, slot=%d\n", level, k, v, t, b, s)
+					//fmt.Printf("Insert: level=%d, key=%d, value=%d, table=%d, bucket=%d, slot=%d\n", level, k, v, t, b, s)
 					c.inserts++
 					c.stats[t].elements++
 					return true
@@ -199,27 +199,28 @@ func (c *Cuckoo) insert(key Key, val Value, level int) (ok bool) {
 			c.bumps++
 			c.stats[t].bumps++
 			victim := rbetween(0, c.nslots-1)
-			fmt.Printf("insert: level=%d, bump value=%d for value=%d, table=%d, bucket=%d, slot=%d\n", level, c.tbs[t][b][victim].val, val, t, b, victim)
+			//fmt.Printf("insert: level=%d, bump value=%d for value=%d, table=%d, bucket=%d, slot=%d\n", level, c.tbs[t][b][victim].val, val, t, b, victim)
 			bucket := c.tbs[t][b][victim]
 			c.tbs[t][b][victim].key = k
 			c.tbs[t][b][victim].val = v
 			k = bucket.key
 			v = bucket.val
 			calcHashes(k)
-			fmt.Printf("insert: level=%d, new key=%d, val=%d\n", level, k, v)
+			//fmt.Printf("insert: level=%d, new key=%d, val=%d\n", level, k, v)
 		}
 		level--
 		if level <= 0 {
 			// sublte bug, on failure to insert the key not inserted may not be the original key
 			// so keep interating until the original key is not found to prevent data loss
 			_, found := c.Lookup(key)
-			fmt.Printf("key %d found=%v\n", key, found)
+			//fmt.Printf("key %d found=%v\n", key, found)
 			if !found {
 				return false
 			}
 		}
 		return ins(k, v)
 	}
+	//fmt.Printf("Insert: level=%d, key=%d, value=%d\n", level, key, val)
 	return ins(key, val)
 }
 
@@ -227,8 +228,9 @@ func (c *Cuckoo) Insert(key Key, val Value) (ok bool) {
 	return c.insert(key, val, *maxLevel)
 }
 
-func trials(tables, buckets, slots, trials int) float64 {
+func trials(tables, buckets, slots, trials int, verbose bool) (float64, int) {
 	tot := float64(0)
+	fails := 0
 	for t := 0; t < trials; t++ {
 		ck := New(tables, buckets, slots)
 		base := 0
@@ -236,25 +238,31 @@ func trials(tables, buckets, slots, trials int) float64 {
 			base = rbetween(1, 1<<29)
 		}
 		cnt := 1
-		maxi := 0
+		maxi := base + tables * buckets * slots
+		percent := float64(1.0)
 		for i := base+1; i <= base + tables * buckets * slots; i++ {
 			if !ck.Insert(Key(i), Value(uint32(cnt))) {
-				percent := float64(ck.inserts)/float64(ck.elements)
-				fmt.Printf("failed: bumps=%d, %d/%d=%0.2f\n", ck.bumps, ck.inserts, ck.elements, percent)
-				for k, v := range ck.stats {
-					fmt.Printf("table[%d]: %d/%d=%0.2f\n", k, v.elements, v.size, float64(v.elements)/float64(v.size))
+				percent = float64(ck.inserts)/float64(ck.elements)
+				if verbose {
+					fmt.Printf("failed: %d/%d, bumps=%d, %d/%d=%0.4f\n", i, maxi, ck.bumps, ck.inserts, ck.elements, percent)
 				}
-				tot += percent
-				maxi = i
+				maxi = i - 1
+				fails++
 				break
 			}
 			cnt++
 		}
-		ck.Verify(base, maxi-1)
+		tot += percent
+		if verbose {
+			for k, v := range ck.stats {
+				fmt.Printf("table[%d]: %d/%d=%0.4f\n", k, v.elements, v.size, float64(v.elements)/float64(v.size))
+			}
+		}
+		ck.Verify(base+1, maxi - base)
 	}
 	avg := tot / float64(trials)
 	//fmt.Printf("tables=%d, buckets=%d, slots=%d, trials=%d, avg=%0.2f\n", tables, buckets, slots, trials, avg)
-	return avg
+	return avg, fails
 }
 
 var auto = flag.Bool("a", false, "automatic")
@@ -282,13 +290,19 @@ func main () {
 
     st := 0
     ss := 0
+    fails := 0
+    verbose := false
+    if *ntrials == 1 {
+    	verbose = true
+    }
     if *auto {
     	max := float64(0)
 		for _, b := range primes.Primes {
     		for t := 1; t <= *ntables; t++ {
     			for s := 1; s <= *nslots; s++ {
 					//fmt.Printf("tables=%d, buckets=%d, slots=%d, trials=%d\n", t, b, s, *ntrials)
-    				avg := trials(t, b, s, *ntrials)
+    				avg, f := trials(t, b, s, *ntrials, verbose)
+    				fails += f
  					//fmt.Printf("tables=%d, buckets=%d, slots=%d, trials=%d\n", t, b, s, *ntrials)
     				if avg > max {
     					max = avg
@@ -297,11 +311,11 @@ func main () {
     				}
     			}
     		}
-	    	fmt.Printf("tables=%d, buckets=%d, slots=%d, trials=%d, avg=%0.2f\n", st, b, ss, *ntrials, max)
+	    	fmt.Printf("tables=%d, buckets=%d, slots=%d, trials=%d, fails=%d, avg=%0.4f\n", st, b, ss, *ntrials, fails, max)
     		max = 0.0
     	}
     } else {
-		avg := trials(*ntables, *nbuckets, *nslots, *ntrials)
-		fmt.Printf("tables=%d, buckets=%d, slots=%d, trials=%d, avg=%0.2f\n", *ntables, *nbuckets, *nslots, *ntrials, avg)
+		avg, f := trials(*ntables, *nbuckets, *nslots, *ntrials, verbose)
+		fmt.Printf("tables=%d, buckets=%d, slots=%d, trials=%d, fails=%d, avg=%0.4f\n", *ntables, *nbuckets, *nslots, *ntrials, f, avg)
 	}
 }
